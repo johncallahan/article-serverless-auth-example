e93473152a6fb491bcaee2756448a1ffcdd2a996
johncallahan <645344+johncallahan@users.noreply.github.com>
2020-03-26 11:46:10 -0400

    Update authenticated-route.js

--- functions/authenticated-route.js
+++ functions/authenticated-route.js
@@ -1,5 +1,27 @@
-const authWrapper = require('../utils/authenticate-wrapper');
+const jwt = require('jsonwebtoken');
 
+const authWrapper = (...args) => {
+    const [context] = args;
+    // using context.queryStringParameters.token as a token holder is for the sake of the demo.
+    // use cookies or an authorization header if you want to use it in prod
+    if(!context.queryStringParameters.token) {
+        return unauthResp(...args);
+    }
+    jwt.verify(context.queryStringParameters.token, 'mySecret', function(err, decoded) {
+        if(err) {
+            return unauthResp(...args);
+        }
+        return fn(...args)
+    });
+};
+
+
+const unauthResp = (event, context, callback) => {
+    callback(null, {
+        statusCode: 400,
+        body: 'Not authenticated',
+    });
+};
 exports.handler = authWrapper(function (event, context, callback) {
     callback(null, {
         statusCode: 200,
